7224f94ae61e8b25e0a80598162faa051b218acf
Fares Ahmed <fares.haliim@gmail.com>
2024-06-05 07:05:37 +0300

    Keep environment variables private 🛅

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyBmn-PzBI3-LeKp2DxywbtmgrhJC7JnC6E",
-  authDomain: "chat-88026.firebaseapp.com",
-  projectId: "chat-88026",
-  storageBucket: "chat-88026.appspot.com",
-  messagingSenderId: "168760086102",
-  appId: "1:168760086102:web:fe6e9f966c4eeaa7ca75f7",
+  apiKey: import.meta.env.VITE_FIREBASE,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
 };
 
 export const app = initializeApp(firebaseConfig);
